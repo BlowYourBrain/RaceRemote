b20f1da988a91ddbb2e8f0a7388f1e7a4d058c43
Васильев Евгений <fetel@yandex.ru>
2021-12-15 11:30:27 +0300

    update library versions

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,20 +2,20 @@
 object Versions {
     //app level
     const val gradle = "7.0.4"
-    const val kotlin = "1.5.21"
+    const val kotlin = "1.6.0"
 
 
     //libs
-    val coreKtx = "1.6.0"
-    val appcompat = "1.3.1"
+    val coreKtx = "1.7.0"
+    val appcompat = "1.4.0"
     val material = "1.4.0"
-    val lifecycle = "2.3.1"
+    val lifecycle = "2.4.0"
 
     //Compose
-    const val compose = "1.0.2"
+    const val compose = "1.1.0-beta04"
     val composeInsets = "0.16.0"
-    val activityCompose = "1.3.1"
-    val navigationCompose = "2.4.0-alpha09"
+    val activityCompose = "1.4.0"
+    val navigationCompose = "2.4.0-beta02"
 
     //test
     val junit = "4.12"

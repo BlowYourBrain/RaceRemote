1c605820f9226e57bb16339c24128f13d78fed52
Evgeny <fetel@yandex.ru>
2022-12-20 21:39:15 +0300

    change fields to constant

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -4,21 +4,20 @@ object Versions {
     const val gradle = "7.0.4"
     const val kotlin = "1.6.10"
 
-
     //libs
-    val coreKtx = "1.7.0"
-    val appcompat = "1.4.1"
-    val material = "1.6.0-rc01"
-    val lifecycle = "2.4.0"
-    val koin = "3.1.6"
+    const val coreKtx = "1.7.0"
+    const val appcompat = "1.4.1"
+    const val material = "1.6.0-rc01"
+    const val lifecycle = "2.4.0"
+    const val koin = "3.1.6"
 
     //Compose
     const val compose = "1.1.1"
-    val activityCompose = "1.4.0"
-    val navigationCompose = "2.4.2"
+    const val activityCompose = "1.4.0"
+    const val navigationCompose = "2.4.2"
 
     //test
-    val junit = "4.12"
-    val extJunit = "1.1.3"
-    val espresso = "3.4.0"
+    const val junit = "4.12"
+    const val extJunit = "1.1.3"
+    const val espresso = "3.4.0"
 }

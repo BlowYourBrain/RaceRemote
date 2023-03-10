977fa9ef357f27558e5f5daa5733c15d586792c6
Evgeny <fetel@yandex.ru>
2023-03-11 01:17:33 +0300

    add new colors

--- app/src/main/java/com/simple/raceremote/ui/theme/Theme.kt
+++ app/src/main/java/com/simple/raceremote/ui/theme/Theme.kt
@@ -8,6 +8,32 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
+    primary = DARK_PRIMARY,
+    onPrimary = DARK_ON_PRIMARY,
+    secondary = DARK_SECONDARY,
+    onSecondary = DARK_ON_SECONDARY,
+    background = DARK_BACKGROUND,
+    onBackground = DARK_ON_BACKGROUND,
+    surface = DARK_SURFACE,
+    onSurface = DARK_ON_SURFACE,
+    error = DARK_ERROR,
+    onError = DARK_ON_ERROR,
+)
+
+private val LightColorPalette = lightColors(
+    primary = LIGHT_PRIMARY,
+    onPrimary = LIGHT_ON_PRIMARY,
+    secondary = LIGHT_SECONDARY,
+    onSecondary = LIGHT_ON_SECONDARY,
+    background = LIGHT_BACKGROUND,
+    onBackground = LIGHT_ON_BACKGROUND,
+    surface = LIGHT_SURFACE,
+    onSurface = LIGHT_ON_SURFACE,
+    error = LIGHT_ERROR,
+    onError = LIGHT_ON_ERROR,
+)
+
+private val LegacyDarkColorPalette = darkColors(
     primary = Color.White,
     primaryVariant = Purple700,
     secondary = Teal200,
@@ -17,7 +43,7 @@ private val DarkColorPalette = darkColors(
     onSurface = LightColor,
 )
 
-private val LightColorPalette = lightColors(
+private val LegacyLightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
     secondary = Teal200
